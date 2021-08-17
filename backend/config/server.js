6a03cdb0a4a103d61d12842e1331d7f46ec80c7b
Diogo Correia <me@diogotc.com>
2021-08-17 23:04:19 +0100

    Add strapi backend (from blog template)

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4b339171f03e77843962505782c4b1cc'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'cac3484c-7bed-4fba-ad06-92ca3ea6a7c4'
+};
